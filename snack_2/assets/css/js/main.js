da0be3cc843e1773b0397b7c338febc19b416a88
Dax2976 <ferrodavide55@gmail.com>
2022-03-30 10:51:25 +0200

    finito snack2

---
+++ snack_2/assets/css/js/main.js
@@ -0,0 +1,19 @@
+//SNACK 2 
+//L’utente inserisce due parole in successione, con due prompt.
+//Il software stampa prima la parola più corta, poi la parola più lunga.
+
+
+let word_1 = prompt('inserisci una parola')
+let word_2 = prompt('insersci un altra parola')
+
+console.log(word_1)
+console.log(word_2)
+
+if (word_1.length > word_2.length){
+    document.querySelector('h1').innerHTML = `la parola ${word_1} è più lunga della parola ${word_2}`
+} else if(word_1.length < word_2.length){
+    document.querySelector('h1').innerHTML = `la parola ${word_2} è più lunga della parola ${word_1}`
+} else if( word_1.length = word_2.length){
+    document.querySelector('h1').innerHTML = `la parola ${word_1} è lunga uguale alla parola ${word_2}`
+}
+
