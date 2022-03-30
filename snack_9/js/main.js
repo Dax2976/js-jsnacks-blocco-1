d5f0126c886a97bfb6d8e2166cc5b438bc0efea2
Dax2976 <ferrodavide55@gmail.com>
2022-03-30 15:13:39 +0200

    snack9  fatto

---
+++ snack_9/js/main.js
@@ -0,0 +1,16 @@
+//calcola la somma di 10 numeri
+//calcola la media di 10 numeri
+
+let numero , somma = 0, media
+
+
+
+for (let i = 0; i < 10; i++) {
+  numero = parseInt(prompt('Inserisci un numero'))
+  somma + numero
+  somma += numero
+  console.log(numero)
+}
+console.log(somma)
+media = somma / 10
+console.log('la media è ', media)
