e4ea7afbf2fedbbf1da762d0a398a34463ef6275
Dax2976 <ferrodavide55@gmail.com>
2022-03-30 11:45:08 +0200

    finito blocco 3

---
+++ snack_3/js/main.js
@@ -0,0 +1,22 @@
+// SNACK 3
+// Il software deve chiedere per 10 volte all’utente di inserire un numero.
+// Il programma stampa la somma di tutti i numeri inseriti.
+
+
+let numbers1 = parseInt(prompt('inserisci un numero'))
+let numbers2 = parseInt(prompt('inserisci un numero'))
+let numbers3 = parseInt(prompt('inserisci un numero'))
+let numbers4 = parseInt(prompt('inserisci un numero'))
+let numbers5 = parseInt(prompt('inserisci un numero'))
+let numbers6 =parseInt(prompt('inserisci un numero'))
+let numbers7 = parseInt(prompt('inserisci un numero'))
+let numbers8 = parseInt(prompt('inserisci un numero'))
+let numbers9 = parseInt(prompt('inserisci un numero'))
+let numbers10 = parseInt(prompt('inserisci un numero'))
+ console.log(numbers1,numbers2,numbers3,numbers4,numbers5,numbers6,numbers7,numbers8,numbers9,numbers10)
+ console.log(numbers1 + numbers2 + numbers3 + numbers4 + numbers5 + numbers6 + numbers7 + numbers8 + numbers9 + numbers10)
+
+ let somma = (numbers1 + numbers2 + numbers3 + numbers4 + numbers5 + numbers6 + numbers7 + numbers8 + numbers9 + numbers10)
+ document.querySelector('h1').innerHTML = `la somma dei tuoi numeri è : ${somma}`
+
+
